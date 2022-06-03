214fcaf29d8e9031db4819ccd034a8644db87c4a
Skyyyr <emuswgluvr@gmail.com>
2022-06-03 13:01:08 -0700

    Finished Assessment

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,76 @@
-# First
+# Constant variables - Only used for the sake of readability -
+PREFIX_OUTPUT = 'The optimal change for an item that costs'
+AMOUNT_GIVEN_OUTPUT = 'with an amount paid of'
+
+def optimal_change(item_cost , amount_paid):
+    """This is a solution to obtaining, and determining the optimal change for any given
+    amount
+
+    Args:
+        item_cost (float): This is our cost of the item.
+        amount_paid (float): This is the 'currency' or 'money' given to pay for the amount, which is higher than the cost of the item.
+
+    Returns:
+        boolean: true or false if the output string is equal to the test case output.
+    """
+
+    # We need to first find out how much change is due, so math
+    # round to fix any of these (0.01999999999999999999999994 would turn into 0.02)
+    change_due = round(amount_paid - item_cost, 2)
+
+    # We built a 'map' that we will traverse to obtain the optimal change
+    # Take note some of the small text short cuts that were taken
+    currency_map = {
+        '$100 bill': 100,
+        '$50 bill': 50,
+        '$20 bill': 20,
+        '$10 bill': 10,
+        '$5 bill': 5,
+        '$1 bill': 1,
+        'quarter': 0.25,
+        'dime': 0.1,
+        'nickle': 0.05,
+        'penny': 0.01,
+    }
+
+    # This is our output string that we initialize with what information we know already
+    output = f"{PREFIX_OUTPUT} ${item_cost} {AMOUNT_GIVEN_OUTPUT} ${amount_paid} is "
+
+    # We now traverse the map starting at the top going down
+    for currency in currency_map:
+        # Initialize a variable that we use to track the amount of the same currency is required
+        amount_of_repeated_currency = 0
+
+        while change_due >= currency_map[currency]:
+            amount_of_repeated_currency += 1
+            change_due -= currency_map[currency]
+            # We need to round to accurately do math
+            change_due = round(change_due, 2)
+
+        # We initialize 2 string variables to assist us in displaying a verbose string
+        plural = ""
+        display_currency = ""
+        if amount_of_repeated_currency > 0:
+            # We set our display now - this is used as a special case catch
+            display_currency = currency
+            # If we have multiples of the same currency used - let's fix our plural string accordingly
+            if amount_of_repeated_currency > 1:
+                # Here's yet another special case catch - 
+                # Penny is the only word that requires additional changes than a simple "s"
+                if (currency == "penny"):
+                    # We already utilize our plural variable, so don't use it when fixing penny
+                    display_currency = "pennie"
+                plural = "s"
+            # Apply our formatted string to the current output
+            output += f"{amount_of_repeated_currency} {display_currency}{plural}, "
+
+    # This is a neat 'trick' - we start from the end of the string and find the first occurrence and replace it
+    # Example of what's happening: replace the first occurrence of ", " with "."
+    output = ".".join(output.rsplit(", ", 1))
+    # We do it again, because we want to put and for our very last currency in the output string
+    # Example of what's happening: replace the first occurrence of ", " with ", and "
+    output = ", and ".join(output.rsplit(", ", 1))
+
+    # For debugging the final output string
+    # print(output)
+    return output
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,15 @@
 # Write your tests here!
+from optimal_change import optimal_change as calc
+
+
+print("1:", calc(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+print("2:", calc(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+# Custom tests
+print("3:", calc(21.99, 100) == "The optimal change for an item that costs $21.99 with an amount paid of $100 is 1 $50 bill, 1 $20 bill, 1 $5 bill, 3 $1 bills, and 1 penny.")
+print("4:", calc(75.38, 200) == "The optimal change for an item that costs $75.38 with an amount paid of $200 is 1 $100 bill, 1 $20 bill, 4 $1 bills, 2 quarters, 1 dime, and 2 pennies.")
+print("5:", calc(98.76, 100) == "The optimal change for an item that costs $98.76 with an amount paid of $100 is 1 $1 bill, 2 dimes, and 4 pennies.")
+print("6:", calc(0.29, 1) == "The optimal change for an item that costs $0.29 with an amount paid of $1 is 2 quarters, 2 dimes, and 1 penny.")
+print("7:", calc(4.55, 5) == "The optimal change for an item that costs $4.55 with an amount paid of $5 is 1 quarter, and 2 dimes.")
+print("8:", calc(4.99, 5) == "The optimal change for an item that costs $4.99 with an amount paid of $5 is 1 penny.")
